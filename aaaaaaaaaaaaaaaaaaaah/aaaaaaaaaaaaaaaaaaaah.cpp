f57360f05447224b77081634a3995ad78afbf3d1
ALUMNO-D104 <ALUMNO-D104@UCSP.AULAB>
2024-11-02 11:52:44 -0500

    updtae

---
+++ aaaaaaaaaaaaaaaaaaaah/aaaaaaaaaaaaaaaaaaaah.cpp
@@ -0,0 +1,90 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct CBinNode
+{
+    CBinNode(int _v)
+    {
+        value = _v; nodes[0] = nodes[1] = 0;
+    }
+    int value;
+    CBinNode* nodes[2];
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class CBinTree
+{
+public:
+    CBinTree();
+    ~CBinTree();
+    bool Insert(int x);
+    void Print();
+private:
+    bool Find(int x, CBinNode**& p);
+    void InOrder(CBinNode* n);
+    CBinNode* m_root;
+};
+
+CBinTree::CBinTree()
+{
+    m_root = 0;
+}
+
+CBinTree::~CBinTree()
+{}
+
+bool CBinTree::Find(int x, CBinNode**& p)
+{
+    for (p = &m_root; *p && (*p)->value != x; p = &((*p)->nodes[(*p)->value < x]));
+    return *p && (*p)->value == x;
+}
+
+bool CBinTree::Insert(int x)
+{
+    CBinNode** p;
+    if (Find(x, p)) return 0;
+    *p = new CBinNode(x);
+    return 0;
+}
+
+void CBinTree::InOrder(CBinNode* n)
+{
+    if (!n) return;
+    InOrder(n->nodes[0]);
+    cout << n->value << " ";
+    InOrder(n->nodes[1]);
+}
+
+void CBinTree::Print()
+{
+    InOrder(m_root);
+    cout << endl;
+}
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
